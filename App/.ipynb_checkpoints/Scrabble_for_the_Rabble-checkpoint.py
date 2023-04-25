1d65ff096eb97c3085e7ef44eaca986de1bb0d9f
brendan <brmc7151@colorado.edu>
2023-04-25 02:30:13 +0000

    troubleshooting

--- App/.ipynb_checkpoints/Scrabble_for_the_Rabble-checkpoint.py
+++ App/.ipynb_checkpoints/Scrabble_for_the_Rabble-checkpoint.py
@@ -69,8 +69,10 @@ def history():
 
 def scoreInsert(score, id):
     print('starting')
+    print(id, " ", score)
     conn = psycopg2.connect("postgres://scrabble_db_user:2JjvW1gU3XXmBbtU3ranf8JX7WBoGfeo@dpg-cgv0079euhlk3uujt5q0-a.oregon-postgres.render.com/scrabble_db")
     cur = conn.cursor()
+    print('connected')
     cur.execute('SELECT MAX(scoreNum) FROM ScoreHistory WHERE userID = %s;', [id])
     maxScoreNum = cur.fetchall()
     maxScoreNum = maxScoreNum[0][0]
@@ -81,6 +83,7 @@ def scoreInsert(score, id):
     minScore = cur.fetchall()
     minScore = minScore[0][0]
     Date = date.today()
+    print(maxSCoreNum, " ", maxScore, " ", minScore)
     if maxScoreNum < 10:
         if score > maxScore:
             for i in range(maxScoreNum, 0, -1):
