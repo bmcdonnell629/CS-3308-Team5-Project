063f6d0d5ac9cc60f028e9262c12e0c436315554
tsanchez19 <105571347+tsanchez19@users.noreply.github.com>
2023-04-26 14:18:17 -0400

    Update Scrabble_for_the_Rabble.py

--- Scrabble_for_the_Rabble.py
+++ Scrabble_for_the_Rabble.py
@@ -24,7 +24,7 @@ def register():
         
         cur.execute('SELECT * FROM form WHERE Username = %s', (Username,))
         account = cur.fetchone()
-        
+        conn.close()
         if account:
             msg = 'Account already exists!'
         elif not re.match(r'[A-Za-z0-9]+', Username):
@@ -32,11 +32,14 @@ def register():
         elif not Username or not Password or not Name:
             msg = 'Please fill out the form'
         else:
+            conn = psycopg2.connect("postgres://scrabble_db_user:2JjvW1gU3XXmBbtU3ranf8JX7WBoGfeo@dpg-cgv0079euhlk3uujt5q0-a.oregon-postgres.render.com/scrabble_db")
+            cur = conn.cursor()
             cur.execute('INSERT INTO Users VALUES (Null, % s, % s, % s)', (Username, Password, Name,))
             msg = 'You have successfully registered'
+            conn.close()
     elif request.method == 'POST':
         msg = 'Please fill out the form'
-    conn.close()
+    
     return render_template('Register_User.html', msg=msg)
 
 @app.route('/about')
