2021002afda3316613bce06d5e7cc7e900e0449f
mutkusnj <96695565+mutkusnj@users.noreply.github.com>
2023-04-27 12:06:48 -0500

    Update Scrabble_for_the_Rabble.py

--- Scrabble_for_the_Rabble.py
+++ Scrabble_for_the_Rabble.py
@@ -49,7 +49,7 @@ def search():
 
 @app.route('/search_results/<search_word>')
 def search_results(search_word=None):
-    return render_template('search_results.html', search_word=search_word)
+    return render_template('search_results.html')
 
 @app.route('/search_history')
 def history():
