c3f2e106be30a4e7e5b3826b8fd6341f5289fed2
tsanchez19 <105571347+tsanchez19@users.noreply.github.com>
2023-04-27 19:12:42 -0400

    Update Scrabble_for_the_Rabble.py

--- Scrabble_for_the_Rabble.py
+++ Scrabble_for_the_Rabble.py
@@ -25,14 +25,14 @@ def register():
     #if request.method == 'POST':
         
         
-        #conn = psycopg2.connect("postgres://scrabble_db_user:2JjvW1gU3XXmBbtU3ranf8JX7WBoGfeo@dpg-cgv0079euhlk3uujt5q0-a.oregon-postgres.render.com/scrabble_db")
-        #cur = conn.cursor()
+    conn = psycopg2.connect("postgres://scrabble_db_user:2JjvW1gU3XXmBbtU3ranf8JX7WBoGfeo@dpg-cgv0079euhlk3uujt5q0-a.oregon-postgres.render.com/scrabble_db")
+    cur = conn.cursor()
         
-        #cur.execute('SELECT* FROM Users WHERE Username = %s', (Username,))
-        #account = cur.fetchone()
-        #conn.close()
-        #if account:
-            #msg = 'Account already exists'
+    cur.execute('SELECT* FROM Users WHERE Username = %s', (Username,))
+    account = cur.fetchone()
+    conn.close()
+    if account:
+        msg = 'Account already exists'
         #elif not re.match(r'[A-Za-z0-9]+', Username):
             #msg = 'Username must contain only either characters and/or numbers'
         #elif not Username or not Password or not Name:
