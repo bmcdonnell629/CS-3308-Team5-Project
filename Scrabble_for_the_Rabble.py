f879903ac0c889ff6cae61b46addb32a2e55664a
tsanchez19 <105571347+tsanchez19@users.noreply.github.com>
2023-04-28 08:32:11 -0400

    Update Scrabble_for_the_Rabble.py

--- Scrabble_for_the_Rabble.py
+++ Scrabble_for_the_Rabble.py
@@ -37,7 +37,7 @@ def register():
         if account:
             msg = 'Username Already Exists, Please Use A Different Username'
         elif not Username or not Password or not Name:
-            mst = 'Please fill out all fields'
+            msg = 'Please fill out all fields'
         else:
             cur.execute('INSERT INTO Users (userID, name, Username, password) VALUES (%s,%s,%s,%s);', (maxUserID+1, Name, Username, Password))    
             conn.commit()
