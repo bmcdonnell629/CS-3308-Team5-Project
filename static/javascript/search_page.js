8176ebb7b90762922b1ca0061b6f3c44d0e6025f
mutkusnj <96695565+mutkusnj@users.noreply.github.com>
2023-04-29 20:44:02 -0400

    Update search_page.js

--- static/javascript/search_page.js
+++ static/javascript/search_page.js
@@ -5,23 +5,59 @@ window.onload = function() {
     about_button.addEventListener("click", about);
     const home_button = document.querySelector("#home_button");
     home_button.addEventListener("click", home);
+    const advanced_button = document.querySelector("#advanced_button");
+    advanced_button.addEventListener("click", advanced);
     const search_button = document.querySelector("#search_button");
     search_button.addEventListener("click", search);
 }
 
-function login(){
+window.addEventListener("keypress", function(event) {    
+    if (event.key == "Enter") {
+        return search();
+    }
+})
+
+function login() {
     location.href="https://scrabble-for-the-rabble.onrender.com/login";
 }
 
-function about(){
+function about() {
     location.href="https://scrabble-for-the-rabble.onrender.com/about";
 }
 
-function home(){
+function home() {
     location.href="https://scrabble-for-the-rabble.onrender.com/";
 }
 
-function search(){
-    const search_word = document.getElementById("search_bar").value;
-    location.href = "https://scrabble-for-the-rabble.onrender.com/search_results?search_word=" + search_word;
+function advanced() {
+    const filters = document.getElementById("advanced_filters");
+    if (filters.style.maxHeight) {
+        filters.style.maxHeight = null;
+        this.style.color = "var(--cyan_after_dark)";
+        this.textContent = "ADVANCED SEARCH +"
+    } else {
+        filters.style.maxHeight = filters.scrollHeight + "px";
+        this.style.color = "var(--spinach)";
+        this.textContent = "ADVANCED SEARCH -"
+    }
+}
+
+function search() {
+    const search_word = document.getElementById("search_bar").value.toUpperCase();
+    const allow_anagrams = document.querySelector(".checkbox").checked;
+    const min_letters = document.getElementById("min_letters").value;
+    const max_letters = document.getElementById("max_letters").value;
+    const starts_with = document.getElementById("start_box").value.toUpperCase();
+    const ends_with = document.getElementById("end_box").value.toUpperCase();
+    const contains = document.getElementById("contain_box").value.toUpperCase();
+    const fixed_letters = document.getElementById("fixed_box").value.toUpperCase();
+    
+    location.href = "https://scrabble-for-the-rabble.onrender.com/search_results?search_word="+search_word+
+        "&allow_anagrams="+allow_anagrams+
+        "&min_letters="+min_letters+
+        "&max_letters="+max_letters+
+        "&starts_with="+starts_with+
+        "&ends_with="+ends_with+
+        "&contains="+contains+
+        "&fixed_letters="+fixed_letters;
 }
