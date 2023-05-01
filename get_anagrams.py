92d36846b6be5bd7f06233a2aa738c3cc59c1605
Nathaniel Mutkus <mutkus@colorado.edu>
2023-05-01 01:58:03 +0000

    Changed scoring function for words
    Changed scoring function to account for fact that blank tiles are worth 0 points.
    QUIZ is worth 22 points
    ?UIZ is worth 12 points

--- get_anagrams.py
+++ get_anagrams.py
@@ -1,6 +1,6 @@
 
 '''
-Modified from free_letter_wordScript.py (original author: Brendan McDonnell)
+Modified from free_letter_wordScript.py (original author: Brendan McDonnell, modified by: Nathaniel Mutkus)
 '''
 
 def characterCount(word):
@@ -15,23 +15,30 @@ def characterCount(word):
         charDict[letter] = charDict.get(letter, 0) + 1
     return charDict
 
-def scrabbleScore(word):
-    '''
-    takes string input of word returns integer if word score
-    '''
-    #initialze score to 0
-    score = 0
-    #dict of scrabble scores per letter
-    scores = {"A": 1, "C": 3, "B": 3, "E": 1, "D": 2, "G": 2, 
-          "F": 4, "I": 1, "H": 4, "K": 5, "J": 8, "M": 3, 
-          "L": 1, "O": 1, "N": 1, "Q": 10, "P": 3, "S": 1, 
-          "R": 1, "U": 1, "T": 1, "W": 4, "V": 4, "Y": 4, 
-          "X": 8, "Z": 10}
-    #iterate by letter through word
-    for letter in word:
-        #add score of current letter to total score
-        score = score + scores[letter]
-    return score
+def search_word_scores(search_word_letters, result_word):
+    letter_scores = {"A": 1, "B": 3, "C": 3, "D": 2, "E": 1,
+                     "F": 4, "G": 2, "H": 4, "I": 1, "J": 8,
+                     "K": 5, "L": 1, "M": 3, "N": 1, "O": 1,
+                     "P": 3, "Q":10, "R": 1, "S": 1, "T": 1,
+                     "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4,
+                     "Z":10, "?": 0}
+    
+    word_score = 0
+    search_dict = search_word_letters
+    
+    for letter in result_word:
+        if letter not in search_dict:
+            if search_dict["?"] > 0:
+                search_dict["?"] = search_dict["?"] - 1
+        elif search_dict[letter] > 0:
+            word_score = word_score + letter_scores[letter]
+            search_dict[letter] = search_dict[letter] - 1
+        elif search_dict["?"] > 0:
+            search_dict["?"] = search_dict["?"] - 1
+        else:
+            print("Letter count is incorrect")
+    
+    return word_score
 
 
 def find_anagrams(user_input):
@@ -56,7 +63,7 @@ def find_anagrams(user_input):
     #scrabble result dictionary with word as key and scabble score as value ex. {'are': '3'...}
     scrabbleList = {}
     #open scrabble dictionary and read each word into list
-    with open('testDictionary.txt') as file:
+    with open('finalWordList.txt') as file:
         for line in file:
             wordList.append(line.rstrip())
     #iterate through list one word at a time
@@ -99,7 +106,7 @@ def find_anagrams(user_input):
         #if flag was not set to 0 then letter list can form that word
         if flag == 1:
             #call funciton to tally word score 
-            score = scrabbleScore(word)
+            score = search_word_scores(characterCount(user_input), word)
             #store {word : score} as key value pair in dictionary ex. {'hello' : 8, etc} 
             scrabbleList[word] = score
     #sort list by score descending order
