dfd01e4c51f31f0b1489b69b30379f85d61a151a
brendan <brmc7151@colorado.edu>
2023-05-02 23:16:22 +0000

    typo

--- .ipynb_checkpoints/Scrabble_for_the_Rabble-checkpoint.py
+++ .ipynb_checkpoints/Scrabble_for_the_Rabble-checkpoint.py
@@ -109,7 +109,7 @@ def show_results():
     result_list = advanced_filters.ends_with_filter(ends_with, result_list)
     result_list = advanced_filters.contains_filter(contains, result_list)
     try:
-        id = int(session.get('id')[0]
+        id = int(session.get('id')[0])
         print(id, search_word, result_list)
         dbInsert.searchInsert(), search_word, result_list)
     except:
